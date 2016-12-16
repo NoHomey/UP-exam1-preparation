e183f21e806ce0516173ee3f4514604dcb8d2ca5
NoHomey <ivo.stratev.tues@gmail.com>
2016-12-16 22:10:13 +0200

    adding task2 gruop a

---
+++ 1000.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int input() {
+    int value;
+    std::cin >> value;
+    return value;
+}
+
+int main() {
+    int array[1000];
+    int n = input();
+    bool result = true;
+    for(size_t i = 0; i < n; ++i) {
+        array[i] = input();
+    }
+    // 0, 1, 2, 3, 4
+    for(size_t i = 1; i <= n; ++i) {
+        if((i % 2) == 0) {
+            if((array[i - 1] % 2) != 0) {
+                result = false;
+                break;
+            } else if((array[i - 1] % i) != 0) {
+                result = false;
+                break;
+            }
+        }
+    }
+    std::cout << (result ? "Yes" : "No") << std::endl;
+
+    return 0;
+}

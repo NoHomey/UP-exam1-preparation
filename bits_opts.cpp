0ad99a2711f6e64116cd1c9ee5383bf3a66aa9b1
NoHomey <ivo.stratev.tues@gmail.com>
2016-12-16 23:05:49 +0200

    addding more opts

--- bits_opts.cpp
+++ bits_opts.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
-//   5 43210
-// [0]01011
-// & true = 0
-// & false = 0
-//   5 43210
-// [1]01011
-// & true = 1
-// & false = 0
+#include <cmath>
 
 void print(int n) {
     if(n != 0) {
@@ -15,11 +8,54 @@ void print(int n) {
     }
 }
 
+int mul(const int n) {
+    return n << 1;
+}
 
+int div(const int n) {
+    return n >> 1;
+}
+
+bool isOdd(const int n) {
+    return n & 1;
+}
+
+bool isEven(const int n) {
+    return !(n & 1);
+}
+
+int addOne(const int n) {
+    if(isEven(n)) {
+        return n | 1;
+    } else {
+        return (n | 2) ^ (n & 1); // dont work, will fix
+    }
+}
+
+/*int reverse(int n) {
+    const int bits = (int)log2(n) + 1;
+    for(int i = 0; i < n; ++i) {
+        n = n ^ (~(n & (1 << i))); 
+    }
+
+    return n;
+}*/
 
 int main() {
     print(5); // 0000101;
     std::cout << std::endl;
-
+    print(mul(5)); // 0001010;
+    std::cout << std::endl;
+    print(div(10)); // 0000101;
+    std::cout << std::endl;
+    print(div(mul(5))); // 0000101;
+    std::cout << std::endl;
+    std::cout << isOdd(5) << std::endl;
+    std::cout << isOdd(6) << std::endl;
+    std::cout << isEven(5) << std::endl;
+    std::cout << isEven(6) << std::endl;
+    std::cout << addOne(5) << std::endl;
+    std::cout << addOne(6) << std::endl;
+    std::cout << addOne(7) << std::endl;
     return 0;
 }
